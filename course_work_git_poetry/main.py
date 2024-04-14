3c596b23f2631a52c15286482103a6adebd8f894
Andrew Kurin <andrew_kurin@mail.ru>
2024-04-15 02:27:15 +0300

    prog without tests is ready

--- course_work_git_poetry/main.py
+++ course_work_git_poetry/main.py
@@ -1,4 +1,8 @@
 import utils
 
-utils.print_exec_str()
+inp_data = utils.input_json_data()
 
+last_5_oper = utils.take_exec_str(inp_data)
+
+for elem in last_5_oper:
+    print(utils.create_output_data(elem))
--- course_work_git_poetry/utils.py
+++ course_work_git_poetry/utils.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import os.path
 
@@ -9,8 +10,49 @@ def input_json_data():
     return data
 
 
-def print_exec_str():
-    for value in input_json_data():
+def take_exec_str(inp_json_data):
+    result = []
+
+    for value in inp_json_data:
         if 'state' in value:
             if value['state'].lower() == 'executed':
-                print(value)
+                result.append(value)
+
+    result.sort(key=lambda x: x['date'], reverse=True)
+
+    return result[:5]
+
+
+def create_output_data(elem: dict):
+    temp_date = datetime.datetime.fromisoformat(elem['date'])
+    date_operation = temp_date.strftime('%d.%m.%Y')
+
+    description_operation = elem['description']
+
+    data_from = from_operation(elem.get('from'))
+    data_to = to_operation(elem['to'])
+    rubles = amount_operation(elem['operationAmount'])
+
+    return (f'{date_operation} {description_operation}\n'
+            f'{data_from} -> {data_to}\n'
+            f'{rubles}\n')
+
+
+def from_operation(inp: str):
+    if inp:
+        *str_data, num = inp.split(' ')
+
+        new_num = f'{num[:4]} {num[4:6]}** **** {num[-4:]}'
+        return f"{' '.join(str_data)} {new_num}"
+
+    else:
+        return ''
+
+
+def to_operation(inp: str):
+    *str_data, num = inp.split(' ')
+    return f'{" ".join(str_data)} **{num[-4:]}'
+
+
+def amount_operation(inp: dict):
+    return f"{inp['amount']} {inp['currency']['name']}"
